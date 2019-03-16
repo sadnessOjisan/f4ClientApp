0b8fde1afd261ad123f5f4af5ba1c7ec3a3cf6b2
sadness_ojisan <sadness.ojisan@gmail.com>
2019-03-17 00:01:00 +0900

    update: mock server

---
+++ src/components/EmployeeItem.jsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import styled from "styled-components";
+
+const Item = props => {
+  return <Wrapper>a</Wrapper>;
+};
+
+const Wrapper = styled.div`
+  width: 100%;
+`;
+
+export default Item;
